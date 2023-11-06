33cba995af5c7c1533bbb8e1296890d71aa2eacc
Andreas Schmidt <andreas.schmidt@hof-university.de>
2023-11-06 16:38:05 +0100

    feat: add repository operation for finding orders by customer ID

--- src/main/java/de/hofuniversity/assemblyplanner/persistence/repository/OrderRepository.java
+++ src/main/java/de/hofuniversity/assemblyplanner/persistence/repository/OrderRepository.java
@@ -1,9 +1,12 @@
 package de.hofuniversity.assemblyplanner.persistence.repository;
 
 import de.hofuniversity.assemblyplanner.persistence.model.Order;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
 public interface OrderRepository extends CrudRepository<Order, UUID> {
+    @Query("SELECT o FROM Order o WHERE o.customer.id = :customerId")
+    Iterable<Order> findByCustomerId(UUID customerId);
 }
