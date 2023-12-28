6ebc24beb40262fed79d823003e2cf0090eb9923
Andreas Schmidt <andreas.schmidt@hof-university.de>
2023-12-28 11:35:54 +0100

    fix: change CustomerQuery address parameter naming

--- src/main/java/de/hofuniversity/assemblyplanner/persistence/model/dto/CustomerQuery.java
+++ src/main/java/de/hofuniversity/assemblyplanner/persistence/model/dto/CustomerQuery.java
@@ -1,5 +1,5 @@
 package de.hofuniversity.assemblyplanner.persistence.model.dto;
 
-public record CustomerQuery(String firstName, String lastName, String company, Integer customerNumber, AddressQuery addressQuery) {
+public record CustomerQuery(String firstName, String lastName, String company, Integer customerNumber, AddressQuery address) {
 
 }
