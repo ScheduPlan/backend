e0008d5a609ae40681b8278aa41a02f2a1c3a4d3
Andreas Schmidt <andreas.schmidt@hof-university.de>
2024-01-24 10:49:12 +0100

    feat: add employee validations

--- src/main/java/de/hofuniversity/assemblyplanner/persistence/model/dto/EmployeeDefinition.java
+++ src/main/java/de/hofuniversity/assemblyplanner/persistence/model/dto/EmployeeDefinition.java
@@ -1,12 +1,14 @@
 package de.hofuniversity.assemblyplanner.persistence.model.dto;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 
 import java.util.UUID;
 
 public record EmployeeDefinition(
         @Valid PersonRequest person,
         @Valid UserDefinition userDefinition,
+        @Min(100000)
         Integer employeeNumber,
         String position,
         UUID teamId
--- src/main/java/de/hofuniversity/assemblyplanner/persistence/model/dto/EmployeeUpdateRequest.java
+++ src/main/java/de/hofuniversity/assemblyplanner/persistence/model/dto/EmployeeUpdateRequest.java
@@ -1,10 +1,12 @@
 package de.hofuniversity.assemblyplanner.persistence.model.dto;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 
 import java.util.UUID;
 
 public record EmployeeUpdateRequest (
+        @Min(100000)
         Integer employeeNumber,
         String position,
         UUID teamId,
