a1ecce9fa865dd9d469802dc5c8e43517145c837
ndrscodes <98schmidtandreas@gmail.com>
2024-02-12 23:09:42 +0100

    refactor: refactor EmployeeControllerTest

--- src/test/java/de/hofuniversity/assemblyplanner/web/EmployeeControllerTest.java
+++ src/test/java/de/hofuniversity/assemblyplanner/web/EmployeeControllerTest.java
@@ -1,7 +1,6 @@
 package de.hofuniversity.assemblyplanner.web;
 
 import de.hofuniversity.assemblyplanner.service.UserService;
-import de.hofuniversity.assemblyplanner.service.api.EmployeeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
