3a2156a1223be2d7053497e3a33b096a268d07c2
Andreas Schmidt <andreas.schmidt@hof-university.de>
2024-02-14 22:52:31 +0100

    fix: return helper events for employees

--- src/main/java/de/hofuniversity/assemblyplanner/persistence/model/dto/EmployeeDto.java
+++ src/main/java/de/hofuniversity/assemblyplanner/persistence/model/dto/EmployeeDto.java
@@ -1,6 +1,7 @@
 package de.hofuniversity.assemblyplanner.persistence.model.dto;
 
 import de.hofuniversity.assemblyplanner.persistence.model.Employee;
+import de.hofuniversity.assemblyplanner.persistence.model.Event;
 
 import java.util.UUID;
 
@@ -11,10 +12,11 @@ public record EmployeeDto (
         Integer employeeNumber,
         String position,
         UUID teamId,
-        UserDto user
+        UserDto user,
+        Iterable<Event> helpsOn
 ) {
     public EmployeeDto(Employee employee) {
         this(employee.getId(), employee.getFirstName(), employee.getLastName(), employee.getEmployeeNumber(),
-                employee.getPosition(), employee.getTeam() == null ? null : employee.getTeam().getId(), new UserDto(employee.getUser()));
+                employee.getPosition(), employee.getTeam() == null ? null : employee.getTeam().getId(), new UserDto(employee.getUser()), employee.getHelpsOn());
     }
 }
