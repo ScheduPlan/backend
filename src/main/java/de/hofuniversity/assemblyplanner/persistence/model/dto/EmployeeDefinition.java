2165c541b39d50754f0d9b247118045e3bc3756d
Andreas Schmidt <andreas.schmidt@hof-university.de>
2024-02-14 20:03:31 +0100

    fix: allow flattened creation of EmployeeDefinition and EmployeeUpdateRequest

--- src/main/java/de/hofuniversity/assemblyplanner/persistence/model/dto/EmployeeDefinition.java
+++ src/main/java/de/hofuniversity/assemblyplanner/persistence/model/dto/EmployeeDefinition.java
@@ -1,8 +1,10 @@
 package de.hofuniversity.assemblyplanner.persistence.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
@@ -14,4 +16,9 @@ public record EmployeeDefinition(
         String position,
         UUID teamId
 
-) { }
+) {
+        @JsonCreator
+        public EmployeeDefinition(@NotNull String firstName, @NotNull String lastName, @Valid UserDefinition userDefinition, @Min(100000) Integer employeeNumber, String position, UUID teamId){
+                this(new PersonRequest(firstName, lastName), userDefinition, employeeNumber, position, teamId);
+        }
+}
--- src/main/java/de/hofuniversity/assemblyplanner/persistence/model/dto/EmployeeUpdateRequest.java
+++ src/main/java/de/hofuniversity/assemblyplanner/persistence/model/dto/EmployeeUpdateRequest.java
@@ -1,8 +1,10 @@
 package de.hofuniversity.assemblyplanner.persistence.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
@@ -12,6 +14,11 @@ public record EmployeeUpdateRequest (
         String position,
         UUID teamId,
         UUID addressId,
-        @Valid @JsonUnwrapped PersonRequest person,
+        @Valid PersonRequest person,
         @Valid UserUpdateRequest user
-) { }
+) {
+        @JsonCreator
+        public EmployeeUpdateRequest(@NotNull String firstName, @NotNull String lastName, @Valid UserUpdateRequest userUpdateRequest, @Min(100000) Integer employeeNumber, String position, UUID teamId, UUID addressId){
+                this(employeeNumber, position, teamId, addressId, new PersonRequest(firstName, lastName), userUpdateRequest);
+        }
+}
